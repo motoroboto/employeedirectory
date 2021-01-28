723f4b8ace7322e9464be609e1863f5f6560f43f
motoroboto <robert.a.cullen@gmail.com>
2021-01-28 00:28:03 -0500

    basic layout

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import '../style.css'
+
+// import "../styles/Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header;
