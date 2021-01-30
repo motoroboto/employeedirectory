ef4dcb13d09678b83042e4c85d07a0d8d61c635d
motoroboto <robert.a.cullen@gmail.com>
2021-01-29 23:26:33 -0500

    style tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../styles/header.css';
 
 const Header = (props) => {
     return (
-    <nav className="navbar content-right">
+    <nav className="navbar shadow content-right">
         <h1>Employee Directory</h1>
         <form className="form-inline m-2 form-control-lg" onSubmit={props.handleFormSubmit}>
             <input
